4fa697284144658a58bf1f522c49c488d2a927b9
fgoerlich <francisco.j.goerlich@gmail.com>
2019-10-11 16:42:28 +0200

    Generalization of the index

--- dist_convergence/README.md
+++ dist_convergence/README.md
@@ -1,9 +1,11 @@
-# Distribution Convergence
+# Catching-up and Distribution Convergence
 
-This Shiny application shows visually an index of convergence between statistical distributions.
+This _Shiny_ application shows visually an index of catching-up and convergence between two statistical distributions.
 
-Given two density functions the convergence between both is mearured by their overlaping area, hence the index is bounded between 0, no overlaping area, and 1, both distributions are identical.
+Given two density functions the convergence between both is measured by an index related to their overlapping area, hence the index is bounded between 0, no overlapping area, and 1, full overlapping, so both distributions are identical.
 
-The user has control over the mean and standard deviation of both distibutions on the left panel, and the overlaping are is shown in red to the right. This area, which is the proposed index, is shown at the bottom of the left panel in percentaje form.
+The user has control over the mean and standard deviation of both distributions on the left panel, and also on the sensibility parameter $\alpha$. The overlapping area is shown in red to the right. This area, together with the intersection area and the catching-up index, in percentaje form, is shown at the bottom of the left panel.
+
+For more details, and real example application, see the research note: **Goerlich, F. J. (2019)** _A simple non-parametric catching-up and convergence index_. Manuscript.
 
 Enjoy!
--- dist_convergence/ui.R
+++ dist_convergence/ui.R
@@ -1,31 +1,35 @@
 library(markdown)
 library(shiny)
 
-shinyUI(navbarPage('Convergence Index',
+shinyUI(navbarPage('Catching-up and Distribution Convergence Index',
   
   tabPanel('Distributions',
     sidebarPanel(
       #   Parameters for the 1st distribution
       sliderInput('mu1', 'Mean distribution 1', value = 10, min = 10, max = 40, step = 1),
       sliderInput('sd1', 'Standard deviation distribution 1', value = 5, min = 1, max = 10, step = 1),
-      br(),br(),
+      br(),
       
       #   Parameters for the 2nd distribution
       sliderInput('mu2', 'Mean distribution 2', value = 40, min = 10, max = 40, step = 1),
       sliderInput('sd2', 'Standard deviation distribution 2', value = 5, min = 1, max = 10, step = 1),
-      
+      br(),
+
+      #   Sensitivity parameter
+      sliderInput('alpha', 'Sensitivity parameter', value = 1, min = 0, max = 3, step = 0.25),
+      br(),
+
       #   Value of the convergence index
-      br(),br(),
-      h5('Value of the convergence index (%)'),
-    	fluidRow(column(6, verbatimTextOutput('index'))),
+      h5('Convergence index (%)'),
+      fluidRow(column(6, verbatimTextOutput('index'))),
       
-    	br(),br(),
-    	helpText(a(href='https://github.com/fgoerlich/Shiny/tree/master/dist_convergence', target='_blank', 'View code'))
+      br(),br(),
+      helpText(a(href = 'https://github.com/fgoerlich/Shiny/tree/master/dist_convergence', target = '_blank', 'View code'))
     ),
     
     mainPanel(
-      h4('Convergence between two distributions as measured by the red area'),
-    	plotOutput('density')
+      h4('Catching-up and Convergence between distributions'),
+      plotOutput('density')
     )
   ),
   
